541eba2f2c23557c20ff77a9706c1d633f414e97
Sujal-skech <sujal8423306557@gmail.com>
2025-09-08 23:59:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/rural-gyan-quest/", // Update this with your actual repo name
+  base: "/", // Update this with your actual repo name
   server: {
     host: "::",
     port: 8080,
